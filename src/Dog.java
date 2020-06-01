f0c11ac841c36edf3b4974914d174d87ae7a927c
Xushaokai <799178096@qq.com>
2020-06-01 11:16:11 +0800

    second commit

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("Dog");
+        test();
+    }
+
+    static void test(){
+        System.out.println("test");
+    }
+}

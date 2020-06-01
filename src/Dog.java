1b93bb368039a9bb66f56ae9f73547a44f5b5d8c
Xushaokai <799178096@qq.com>
2020-06-01 12:05:57 +0800

    ddd

--- src/Dog.java
+++ src/Dog.java
@@ -2,9 +2,14 @@ public class Dog {
     public static void main(String[] args) {
         System.out.println("Dog");
         test();
+        from();
     }
 
     static void test(){
         System.out.println("test");
+    }
+
+    static void from(){
+        System.out.println("from");
     }
 }
